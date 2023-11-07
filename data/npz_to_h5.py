1c392fd8b5eb97cfb4e395557a67ca5ce9923820
AntoineMsy <antoinemisery8@gmail.com>
2023-11-07 15:13:51 +0100

    corrected npz to h5

--- data/npz_to_h5.py
+++ data/npz_to_h5.py
@@ -7,10 +7,10 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("", "--input_folder", help="Path to the input folder", default="/scratch2/sfgd/sparse_data_genie_fhc_numu_hittag/")
+    parser.add_argument("-i", "--input_folder", help="Path to the input folder", default="/scratch2/sfgd/sparse_data_genie_fhc_numu_hittag/")
    
     parser.add_argument("-o", "--output_file", help="Path to the output file", default="out.h5")
-    parser.add_argument("-l", "max_event_length", default=5000)
+    parser.add_argument("-l", "--max_evt_length", type=int, default=5000)
     args = parser.parse_args()
     return args
 
@@ -22,23 +22,20 @@ if __name__ == "__main__":
 
     total_rows = 0
     total_hits = 0
-    min_hits = 1
-    max_event_length = config.max_event_length
+    
+    max_event_length = config.max_evt_length
 
     print("counting events and hits, in files")
-
+    good_idx = []
     for i in tqdm.tqdm(range(N_files)):
         npz_file = np.load(data_path + "event%d.npz"%i)
 
         coords = npz_file["c"]
         if len(coords) <= max_event_length:
+            good_idx.append(i)
             total_rows += 1
             total_hits += len(coords)
 
-            if i == 0 :
-                min_hits = len(coords)
-            elif len(coords) < min_hits:
-                min_hits = len(coords)
         
             
         
@@ -67,10 +64,10 @@ if __name__ == "__main__":
     hit_offset_next = 0
 
     print("Creating dataset")
-
-    for i in tqdm.tqdm(range(N_files)):
+    
+    for i in tqdm.tqdm(good_idx):
         npz_file = np.load(data_path + "event%d.npz"%i)
-        dset_event_hit_index[i] = hit_offset
+        dset_event_hit_index[offset] = hit_offset
         coords = npz_file["c"]
         cat = npz_file["y"]
         time = npz_file["x"][:,0]
@@ -82,8 +79,8 @@ if __name__ == "__main__":
         dset_hit_coords[hit_offset:hit_offset_next] = coords
         dset_hit_charge[hit_offset:hit_offset_next] = charge
         dset_hit_labels[hit_offset:hit_offset_next] = cat
-        dset_vertex_pos[i] = vpos
-
+        dset_vertex_pos[offset] = vpos
+        offset+=1
         hit_offset = hit_offset_next
 
     f.close()
